fa7e18b4951c6acc82ce16a3147bb40ffcff0a6e
Karol Sluszniak <ksluszniak@gmail.com>
2017-11-13 14:16:53 +0100

    Fix status reporting

--- apps/click-api/docker/test-isolated/start.sh
+++ apps/click-api/docker/test-isolated/start.sh
@@ -5,6 +5,10 @@ while ! nc -q0 db 5432; do sleep 1; done
 
 echo "Running tests (mix test)..."
 mix test
+test_status=$?
 
 echo "Checking code quality (mix credo)..."
 mix credo
+credo_status=$?
+
+exit $(( test_status + credo_status ))
